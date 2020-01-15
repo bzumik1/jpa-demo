788a6d2c5fe7107701a3bcb061b6a41663cd5dca
Jakub Znamenáček <bzumik1@users.noreply.github.com>
2020-01-15 16:57:37 +0100

    start

---
+++ src/main/java/com/znamenacek/jakub/Employee.java
@@ -0,0 +1,15 @@
+package com.znamenacek.jakub;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter @Setter @ToString @NoArgsConstructor @AllArgsConstructor
+@Entity
+public class Employee {
+    @Id
+    private Integer id;
+    private String name;
+    private Integer salary;
+}
---
+++ src/main/java/com/znamenacek/jakub/Main.java
@@ -0,0 +1,55 @@
+package com.znamenacek.jakub;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //create instance of Employee
+        Employee employee01 = new Employee(1,"Jakub",10000);
+        Employee employee02 = new Employee(2,"Petr",10000);
+
+        //create entity manager and entity manager factory
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("EmployeeService"); // connect with persistence.xml
+        EntityManager em = emf.createEntityManager(); //create entityManager
+
+        //persist entity
+        em.getTransaction().begin(); //starts transaction
+        em.persist(employee01);
+        em.persist(employee02);
+        em.getTransaction().commit(); //ends transaction
+
+
+        //load from database
+        // we are passing in the class of the entity and ID (primary key)
+        // returns null if not found
+        Employee employee03 = em.find(Employee.class, 1); //doesn't have to be processed as transaction because it only reads from DB
+        System.out.println(employee03);
+
+        //remove from database
+        em.getTransaction().begin(); //starts transaction
+        var employee04 = em.find(Employee.class,1); // I have to look ob the employee with given id is in DB, otherwise throws an error
+        if(employee04!=null){
+            em.remove(employee04); // remove employee from DB
+        }
+        em.getTransaction().commit(); //ends transaction
+
+        //updating value in database
+        em.getTransaction().begin(); //starts transaction
+        var employee05 = em.find(Employee.class,2);
+        if(employee05!=null){
+            employee05.setSalary(employee05.getSalary()+1000); //entity is managed instance, so it doesn't have to be saved back to DB
+        }
+        em.getTransaction().commit(); //ends transaction
+
+        //query example
+        TypedQuery<Employee> query = em.createQuery("SELECT employee FROM Employee employee", Employee.class); // employee is variable
+        //List<Employee> employees = query.getResultList();
+        System.out.println("QUERY");
+        query.getResultStream().forEach(System.out::println);
+
+    }
+}
